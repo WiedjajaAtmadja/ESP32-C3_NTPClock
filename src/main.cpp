db02a51d23c36cd602d5b8c916c1dfc87f819e72
Wiedjaja Atmadja <swiedjaja@binus.ac.id>
2024-09-25 11:08:43 +0700

    Step 2. Connect to NTP

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 #include "wifi_id.h"
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, 7*3600); // UTC+7
 
 void setup() {
   // put your setup code here, to run once:
@@ -15,9 +20,13 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+
+  timeClient.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  timeClient.update();
+  Serial.println(timeClient.getFormattedTime());  
   delay(1000); // this speeds up the simulation
 }
